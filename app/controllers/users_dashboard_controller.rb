7986ef098e9d600a428b6985816087a5a3986162
John Van Arkelen <johnvanarkelen@gmail.com>
2012-11-06 21:41:27 +0100

    latest

--- app/controllers/users_dashboard_controller.rb
+++ app/controllers/users_dashboard_controller.rb
@@ -1,7 +1,7 @@
 class UsersDashboardController < ApplicationController
   def show
   	# Find first expiring insurances
-    @insurances = current_user.company.ins_insurances.order("main_premium_expiration_date ASC").limit(15)
+    @insurances = current_user.company.ins_insurances.where("first_contract_expiration_date >= ?", Date.today).order("first_contract_expiration_date ASC").limit(25)
   end
 
 end
