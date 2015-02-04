4767808102d64ae84c56e49b195a9aba485abad1
John van Arkelen <johnvanarkelen@gmail.com>
2015-02-04 15:49:23 +0100

    added log level

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ CustomerFirst::Application.configure do
   config.force_ssl = false
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :info
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
